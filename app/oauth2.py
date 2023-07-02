c175a7424e1b96dd8a71afe305fdee9d8fe6dc52
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2023-07-02 21:31:23 +0900

    環境変数をENVファイルへ移動

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,13 +1,13 @@
 from jose import jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
+import os
 
-SECRET_KEY = "70b3841b672447e142aa8e9811f0bd0ea92b6a0f24a0d1c273f90e0e2a1b55f1" # $ openssl rand -hex 32
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+load_dotenv()
 
 def create_access_token(data: dict):
     payload = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now() + timedelta(minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")))
     payload.update({"exp": expire})
-    result = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+    result = jwt.encode(payload, os.getenv("SECRET_KEY"), algorithm=os.getenv("ALGORITHM"))
     return result
